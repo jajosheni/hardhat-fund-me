cee40f0d2c11a40afc6a98bbc69acea1ba9d964d
Sheni Hamitaj <shen.i@live.com>
2023-02-01 00:39:44 +0100

    --add: verification

--- deploy/01-fund-me.js
+++ deploy/01-fund-me.js
@@ -3,6 +3,7 @@ const {
   developmentChains,
 } = require("../helper-hardhat.config");
 const { network } = require("hardhat");
+const { verify } = require("../utils/verify");
 
 module.exports = async ({ deployments, getNamedAccounts }) => {
   const { deploy, log, get } = deployments;
@@ -17,11 +18,20 @@ module.exports = async ({ deployments, getNamedAccounts }) => {
     ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
   }
 
+  const args = [ethUsdPriceFeedAddress];
   const fundMe = await deploy("FundMe", {
     from: deployer,
-    args: [ethUsdPriceFeedAddress],
+    args: args,
     log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
   });
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(fundMe.address, args);
+  }
 
   log("--------------------------------------------");
 };
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,8 @@ require("hardhat-deploy");
 require("dotenv").config();
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const PRIVATE_KEY_1 = process.env.PRIVATE_KEY_1 || "0xkey";
+const PRIVATE_KEY_2 = process.env.PRIVATE_KEY_2 || "0xkey";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
 
@@ -17,13 +18,22 @@ module.exports = {
   networks: {
     goerli: {
       url: GOERLI_RPC_URL,
-      accounts: [PRIVATE_KEY],
+      accounts: [PRIVATE_KEY_1, PRIVATE_KEY_2],
       chainId: 5,
+      blockConfirmations: 6,
     },
     localhost: {
       url: "http://127.0.0.1:8545/",
       chainId: 31337,
     },
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC",
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
